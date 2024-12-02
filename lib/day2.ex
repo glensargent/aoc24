fe862f3e9057a05c691e81d83bfdabd6ff4a23af
Glen Sargent <glensarg@hotmail.co.uk>
2024-12-02 17:37:20 +0000

    finish part 2

--- lib/day2.ex
+++ lib/day2.ex
@@ -14,11 +14,23 @@ defmodule Aoc25.Day2 do
     diff >= 1 and diff <= 3 and valid_differences?([b | rest])
   end
 
-  def valid_level?(level) do
+  defp safe_with_one_removed?(level) do
+    Enum.any?(0..(length(level) - 1), fn x ->
+      level
+      |> List.delete_at(x)
+      |> valid_level?()
+    end)
+  end
+
+  defp valid_level?(level) do
     all_increasing = increasing?(level)
     all_decreasing = decreasing?(level)
     valid_differences = valid_differences?(level)
     (all_increasing or all_decreasing) and valid_differences
+  end
+
+  defp safe_with_dampener?(level) do
+    valid_level?(level) or safe_with_one_removed?(level)
   end
 
   def parse(input) do
@@ -30,7 +42,7 @@ defmodule Aoc25.Day2 do
       Enum.map(lvl, &String.to_integer(&1))
     end)
     |> Enum.map(fn lvl ->
-      valid_level?(lvl)
+      safe_with_dampener?(lvl)
     end)
     |> Enum.filter(&(&1 == true))
     |> Enum.count()
